31ad1877ebdb49e4d467f3a6ab43208a32c6d626
Piotr Brzezinski (PIBN) <pibn@senncom.com>
2022-11-29 21:17:40 +0100

    Add initial migrations support on data loading Start adding Prometheus support

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import matplotlib.dates as mdate
 import matplotlib.ticker as mtick
 import numpy as np
 import random as rand
+import pickle
+import re
+from prometheus_api_client import PrometheusConnect
 
 class VmUsageRecord:
 
@@ -70,13 +73,20 @@ class Node:
         self.vms = []
         self.utilization = []
         self.aggregate_utilization = []
-        self.vcpu_ratio = 4
+        self.migrations = []
     
     def add_usage_record(self, record:NodeUsageRecord):
         self.utilization.add(record)
 
+    # Add VM to a node. If it already exists, include missing usage records
     def add_vm(self, vm):
-        self.vms.append(vm)
+        try:
+            _, existing_vm = self.get_vm_by_vmid(vm.id)
+            for index, utilization_record in existing_vm.utilization:
+                if utilization_record.cpu == 0:
+                    existing_vm.utilization[index] = vm.utilization[index]
+        except:
+            self.vms.append(vm)
     
     def get_utilization_by_timestamp(self, timestamp) -> NodeUsageRecord:
         for index, record in enumerate(self.utilization):
@@ -87,9 +97,10 @@ class Node:
 
     def get_vm_by_vmid(self, vmid):
         for index, vm in enumerate(self.vms):
-            if vm.id == vmid:
+            if str(vm.id) == str(vmid):
                 return index, vm
         raise Exception("There are is no vm with id {0} on {1} node".format(vmid, self.name))
+
 
     # Recalculates aggregate cpu utilization of all vms
     def calculate_aggregate_utilization(self):
@@ -97,9 +108,12 @@ class Node:
         for i in range(0, len(self.utilization)):
             cpu_utilization_sum = 0
             for vm in self.vms:
-                _, current_vm_utilization_record = vm.get_utilization_by_timestamp(self.utilization[i].time)
-                # Include number of cores in calculation
-                cpu_utilization_sum += (current_vm_utilization_record.cpu * current_vm_utilization_record.maxcpu)/self.vcpu_ratio
+                try:
+                    _, current_vm_utilization_record = vm.get_utilization_by_timestamp(self.utilization[i].time)
+                    # Include number of cores in calculation
+                    cpu_utilization_sum += (current_vm_utilization_record.cpu * current_vm_utilization_record.maxcpu/self.utilization[i].maxcpu)
+                except:
+                    pass                
             aggregate_utilization.append(cpu_utilization_sum)
         self.aggregate_utilization = aggregate_utilization
 
@@ -153,12 +167,15 @@ class Cluster:
         for i in range(0, len(self.nodes)):
             self.nodes[i].calculate_aggregate_utilization()
             for record in self.nodes[i].aggregate_utilization:
-            #    if record > 1.0:
-            #        total_sum += 1
-            #    else:
                     total_sum += record
         return total_sum/(len(self.nodes[0].aggregate_utilization * len(self.nodes)))
     
+    def get_node_by_name(self, name):
+        for node in self.nodes:
+            if node.name == name:
+                return node
+        raise Exception("There are is no node with name {0} on {1} cluster".format(name, self.name))
+
     # Get names of all nodes
     def get_node_names(self):
         node_names = []
@@ -176,87 +193,128 @@ def load_cluster_info(timeframe="week", cf="AVERAGE") -> Cluster:
     config = yaml.safe_load(open("config.yaml"))
 
     # Initialize ProxmoxAPI
+    if config["method"] == "proxmox":
+        proxmox = ProxmoxAPI(config["connection"]["proxmox"]["url"]["ip"], user=config["connection"]["proxmox"]["auth"]["username"], password=config["connection"]["proxmox"]["auth"]["password"], verify_ssl=False)
 
-    proxmox = ProxmoxAPI(config["connection"]["url"]["ip"], user=config["connection"]["auth"]["username"], password=config["connection"]["auth"]["password"], verify_ssl=False)
+        nodes_list = proxmox.nodes.get()
+        for node in nodes_list:
+            
+            current_node = Node(node["node"])
 
-    nodes_list = proxmox.nodes.get()
-    for node in nodes_list:
-        
-        current_node = Node(node["node"])
-
-        # Gather and add node utilization history
-        node_usage_data = proxmox.nodes(node["node"]).rrddata.get(timeframe=timeframe, cf=cf)
-        for data_entry in node_usage_data:
-            current_node.utilization.append(
-                NodeUsageRecord(
-                    data_entry["time"],
-                    data_entry["maxcpu"],
-                    data_entry["netout"],
-                    data_entry["netin"],
-                    data_entry["swapused"],
-                    data_entry["swaptotal"],
-                    data_entry["memtotal"],
-                    data_entry["memused"],
-                    data_entry["loadavg"],
-                    data_entry["iowait"],
-                    data_entry["rootused"],
-                    data_entry["cpu"],
-                    data_entry["roottotal"]
-                )
-            )
-
-        # Gather assignment of VMs to Nodes
-        for vm in proxmox.nodes(node["node"]).qemu.get():
-
-            current_vm = VM(vm["vmid"])
-
-            # TODO: Whether to only check running VMs?
-            if vm["status"] == "stopped":
-                continue
-
-            vm_usage_data = proxmox.nodes(node["node"]).qemu(vm['vmid']).rrddata.get(timeframe="week", cf="AVERAGE")
-            for data_entry in vm_usage_data:
-                try:
-                    current_vm.utilization.append(
-                        VmUsageRecord(
-                            data_entry["time"],
-                            data_entry["disk"],
-                            data_entry["maxdisk"],
-                            data_entry["cpu"],
-                            data_entry["maxcpu"],
-                            data_entry["netin"],
-                            data_entry["netout"],
-                            data_entry["diskread"],
-                            data_entry["diskwrite"],
-                            data_entry["mem"],
-                            data_entry["maxmem"]
-                        )
-                    )
-                except:
-                    # Data entry doesn't fit schema
-                    print("Data entry doesn't fit the schema for:\n{0}\nAdding empty usage.".format(vm["name"]))
-                    for key in data_entry.keys():
-                        print("\t{0}: {1}".format(key,data_entry[key]))
-                    current_vm.utilization.append(VmUsageRecord(
+            # Gather and add node utilization history
+            node_usage_data = proxmox.nodes(node["node"]).rrddata.get(timeframe=timeframe, cf=cf)
+            for data_entry in node_usage_data:
+                current_node.utilization.append(
+                    NodeUsageRecord(
                         data_entry["time"],
-                        0,
-                        0,
-                        0,
-                        0,
-                        0,
-                        0,
-                        0,
-                        0,
-                        0,
-                        0
-                    ))
-            # Add VM to corresponding node
-            current_node.vms.append(current_vm)
-        cluster.nodes.append(deepcopy(current_node))
-        del current_node
+                        data_entry["maxcpu"],
+                        data_entry["netout"],
+                        data_entry["netin"],
+                        data_entry["swapused"],
+                        data_entry["swaptotal"],
+                        data_entry["memtotal"],
+                        data_entry["memused"],
+                        data_entry["loadavg"],
+                        data_entry["iowait"],
+                        data_entry["rootused"],
+                        data_entry["cpu"],
+                        data_entry["roottotal"]
+                    )
+                )
+
+            # Gather assignment of VMs to Nodes
+            for vm in proxmox.nodes(node["node"]).qemu.get():
+
+                current_vm = VM(vm["vmid"])
+
+                # TODO: Whether to only check running VMs?
+                if vm["status"] == "stopped":
+                    continue
+
+                vm_usage_data = proxmox.nodes(node["node"]).qemu(vm['vmid']).rrddata.get(timeframe=timeframe, cf=cf)
+                for data_entry in vm_usage_data:
+                    try:
+                        current_vm.utilization.append(
+                            VmUsageRecord(
+                                data_entry["time"],
+                                data_entry["disk"],
+                                data_entry["maxdisk"],
+                                data_entry["cpu"],
+                                data_entry["maxcpu"],
+                                data_entry["netin"],
+                                data_entry["netout"],
+                                data_entry["diskread"],
+                                data_entry["diskwrite"],
+                                data_entry["mem"],
+                                data_entry["maxmem"]
+                            )
+                        )
+                    except:
+                        # Data entry doesn't fit schema
+                        print("Data entry doesn't fit the schema for:\n{0}\nAdding empty usage.".format(vm["name"]))
+                        for key in data_entry.keys():
+                            print("\t{0}: {1}".format(key,data_entry[key]))
+                        current_vm.utilization.append(VmUsageRecord(
+                            data_entry["time"],
+                            0,
+                            0,
+                            0,
+                            0,
+                            0,
+                            0,
+                            0,
+                            0,
+                            0,
+                            0
+                        ))
+                # Add VM to corresponding node
+                current_node.vms.append(current_vm)
+
+            # Read log for node to find and simulate any migrations performed
+            # Requires Sys.audit permissions to display all logs
+            reference_epoch = current_node.utilization[0].time
+            logs = proxmox.nodes(node["node"]).tasks.get(typefilter="qmigrate", since=reference_epoch)
+            for migration in logs:
+                # Find target node
+                log_text = proxmox.nodes(node["node"]).tasks(migration["upid"]).log.get()
+                migration["target_node"] = re.findall(r"'(.+)'", log_text[0]["t"])[0]
+            current_node.migrations = logs
+            
+            cluster.nodes.append(deepcopy(current_node))
+        
+        # For each migration, include the VM in both source and target nodes, but split utilization records depending on the migration timestamp
+        # Migration format example:
+        # {'endtime': 1669728729, 'status': 'OK', 'pstart': 1435759933, 'upid': 'UPID:pve-g2n6:00343D2D:5593F53D:638609B1:qmigrate:114:pibn@pve:', 'starttime': 1669728689, 'type': 'qmigrate', 'node': 'pve-g2n6', 'target_node': 'pve-g2n8', 'user': 'pibn@pve', 'pid': 3423533, 'id': '114'}
+        
+        initial_migrations = []
+        for node in cluster.nodes:
+            for migration in node.migrations:
+                initial_migrations.append(migration)
+        initial_migrations.sort(key=lambda a: a["endtime"], reverse=True)
+        for migration in initial_migrations:
+            # Initial migrations simulated from last to first
+            source_node = cluster.get_node_by_name(migration["node"])
+            target_node = cluster.get_node_by_name(migration["target_node"])
+            _, target_vm = target_node.get_vm_by_vmid(migration["id"])
+            
+            # Check if VM records already exist on source node
+            current_vm = deepcopy(target_vm)
+
+            migration_timestamp_index = 0       # Index of record when migration occured, 0-69
+            for index, utilization_record in enumerate(target_vm.utilization):
+                if utilization_record.time < migration["endtime"]:
+                    # Remove utilization records from before migration
+                    target_vm.utilization[index].cpu = 0
+                else:
+                    migration_timestamp_index = index
+                    continue
+            for index in range(migration_timestamp_index, len(current_vm.utilization)):
+                current_vm.utilization[index].cpu = 0
+            source_node.add_vm(current_vm)
+                  
+    elif config.method == "prometheus":
+        prom = PrometheusConnect(url ="<prometheus-host>", disable_ssl=True)
     return cluster
-
-
 
 def plot_cluster_cpu_usage(cluster: Cluster, node_names_list = []):
     
@@ -321,26 +379,28 @@ def plot_cluster_cpu_usage(cluster: Cluster, node_names_list = []):
     print("Average cluster utilization is: {0}".format(total_average))
     ax[n_nodes].axhline(y = total_average, color = 'y', linestyle = '-')
     ax[n_nodes].axhline(y = 1, color = 'r', linestyle = '-')
-    plt.show()
 
 def migrate(vmid, source_host: Node, target_host: Node):
     # Move vm from source to host
 
-    # Modify utilization records accordingly
     vm_index, vm = source_host.get_vm_by_vmid(vmid)
 
-    target_host.add_vm(deepcopy(vm))
+    current_vm = deepcopy(vm)
+
+    # Modify utilization records accordingly 
+
+    # Normalize utilization to match target host CPUs
+    cpu_factor = target_host.utilization[0].maxcpu/source_host.utilization[0].maxcpu
+    for record in current_vm.utilization:
+        record.cpu = record.cpu * cpu_factor
+
+    # Adjust cpu usage on source node
     del source_host.vms[vm_index]
-
-    #print("Migrated vm: {0}\n\tfrom: {1}\n\tto: {2}".format(vmid, source_host.name, target_host.name))
     
-    #for count, record in enumerate(vm.utilization):
-        # Reduce usage on source host
-        #index, node_record = source_host.get_utilization_by_timestamp(record.time)
-        #print("Count: {0}, timestamp: {1}\n\t".format(count, record.time))
-        #print(record)
-        #print(node_record)
 
+    # Adjust cpu usage on target node
+    target_host.add_vm(current_vm)
+    
 def perform_migrations(cluster: Cluster):
 
     # Find time at which aggregated utilization is over 100%
@@ -391,33 +451,46 @@ def random_migration(cluster: Cluster) -> Cluster:
 
 def main():
     start_time = time.time()
-    cluster = load_cluster_info()
+    
+    load_method = "API"
+    timeframe = "day"
+    save = False
+    
+    if load_method == "FILE":
+        # Load from file
+        with open('snapshots/scenario1/29112022145310-snapshot-before-hour', 'rb') as cluster_file:
+            cluster = pickle.load(cluster_file)
+    else:
+        # Load from API
+        cluster = load_cluster_info(timeframe,"AVERAGE")
     print("Cluster information loaded in --- %s seconds ---" % round(time.time() - start_time,3))
-
-    #for vm in cluster.nodes[0].vms:
-        #for record in vm.utilization:
-            #print(record.cpu)
-            #print(record.maxcpu)
+    
+    # Save snapshot
+    if save:
+        with open("snapshots/scenario2/{0}-snapshot-before-{1}".format(datetime.today().strftime("%d%m%Y%H%M%S"), timeframe), 'wb') as cluster_snapshot:
+            pickle.dump(cluster, cluster_snapshot)
 
     score = cluster.get_average_cpu_usage()
     start_score = score
     print(score)
     
     start_time = time.time()
-    n_iterations = 2
-    for i in range(0, n_iterations):
-        new_state = random_migration(cluster)
-        new_state_score = new_state.get_average_cpu_usage()
-        if  new_state_score > score:
-            score = new_state_score
-            cluster = new_state
+    n_iterations = 0
+    plot_cluster_cpu_usage(cluster, ["pve-g2n6", "pve-g2n8"])
+    #migrate(114, cluster.get_node_by_name("pve-g2n6"), cluster.get_node_by_name("pve-g2n8"))
     #Recalculate score
     score = cluster.get_average_cpu_usage()
     #plot_cluster_cpu_usage(cluster)
     print("Migrations performed in --- %s seconds ---" % round(time.time() - start_time,3))
     print("Improvement from: {0} to {1}. (by {2}%)".format(start_score, score, (score-start_score)/start_score*100))
 
+    # Case 1, test cluster:
     plot_cluster_cpu_usage(cluster, ["pve-g2n6", "pve-g2n8"])
+    # Case 2, production cluster:
+    #plot_cluster_cpu_usage(cluster)
+
+    # Plot all figures
+    plt.show()
 
 if __name__ == "__main__":
     main()
